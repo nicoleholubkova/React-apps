7cc90e5482698e88a90ef3dd1a412204901c717e
holubica <holubkova.nicola@gmail.com>
2021-07-10 14:17:27 +0200

    inline styles added, my app is complete now

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,26 +1,57 @@
 import React from "react";
 
+const styles = {
+  h1: {
+    textAlign: "center",
+    fontFamily: "Brush Script MT, cursive",
+    padding: "40px 0",
+    color: "#22577A",
+  } as const,
+  button: {
+    textAlign: "center",
+    fontFamily: "Helvetica, Sans-serif",
+    padding: "10px 20px",
+    margin: "8px",
+    position: "relative",
+    left: "calc(50% - 141px)",
+    textTransform: "uppercase",
+    background: "#38A3A5",
+    color: "white",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontWeight: "bold",
+  } as const,
+  body: {
+    backgroundColor: "#C7F9CC",
+    margin: "160px 340px",
+    padding: "20px 20px 65px",
+    borderRadius: "50%",
+  },
+};
+
 export class MyStateComp extends React.Component {
   state = {
     number: 1,
   };
   render() {
     return (
-      <div>
-        <h1>Current number {this.state.number}</h1>
+      <div style={styles.body}>
+        <h1 style={styles.h1}>Your current number is: {this.state.number}</h1>
         <button
+          style={styles.button}
           onClick={() => {
             this.setState({ number: this.state.number + 1 });
           }}
         >
-          Add 1
+          Increment
         </button>
         <button
+          style={styles.button}
           onClick={() => {
             this.setState({ number: this.state.number - 1 });
           }}
         >
-          Remove 1
+          Decrement
         </button>
       </div>
     );
