9a698e9ce4d939d63a6bf14ce42a9c2619d07c6c
holubica <holubkova.nicola@gmail.com>
2021-07-12 11:31:16 +0200

    setState changed, theme.ts added and number rewrite to count

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,46 +1,53 @@
+import { Theme } from "./theme";
 import React from "react";
 
 const styles = {
   h1: {
     textAlign: "center",
-    fontFamily: "Brush Script MT, cursive",
+    fontFamily: Theme.quinary,
     padding: "40px 0",
-    color: "#22577A",
+    color: Theme.primaryColor,
   } as const,
   button: {
     textAlign: "center",
-    fontFamily: "Helvetica, Sans-serif",
+    fontFamily: Theme.senary,
     padding: "10px 20px",
     margin: "8px",
     position: "relative",
     left: "calc(50% - 141px)",
     textTransform: "uppercase",
-    background: "#38A3A5",
-    color: "white",
+    background: Theme.secondaryColor,
+    color: Theme.quaternaryColor,
     borderRadius: "5px",
     cursor: "pointer",
     fontWeight: "bold",
   } as const,
   body: {
-    backgroundColor: "#C7F9CC",
+    backgroundColor: Theme.tertiaryColor,
     margin: "160px 340px",
     padding: "20px 20px 65px",
     borderRadius: "50%",
   },
 };
 
-export class MyStateComp extends React.Component {
-  state = {
-    number: 1,
-  };
+export class MyStateComp extends React.Component<{}, { count: number }> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+    };
+  }
+
   render() {
     return (
       <div style={styles.body}>
-        <h1 style={styles.h1}>Your current number is: {this.state.number}</h1>
+        <h1 style={styles.h1}>Your current number is: {this.state.count}</h1>
         <button
           style={styles.button}
           onClick={() => {
-            this.setState({ number: this.state.number + 1 });
+            this.setState((state) => ({
+              count: state.count + 1,
+            }));
           }}
         >
           Increment
@@ -48,7 +55,9 @@ export class MyStateComp extends React.Component {
         <button
           style={styles.button}
           onClick={() => {
-            this.setState({ number: this.state.number - 1 });
+            this.setState((state) => ({
+              count: state.count - 1,
+            }));
           }}
         >
           Decrement
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+export const Theme = {
+  primaryColor: "#22577A",
+  secondaryColor: "#38A3A5",
+  tertiaryColor: "#C7F9CC",
+  quaternaryColor: "white",
+  quinary: "Brush Script MT, cursive",
+  senary: "Helvetica, Sans-serif",
+};
